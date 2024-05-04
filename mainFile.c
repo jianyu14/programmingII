ce84ca6c13128c385bc28121d06aae85d176d640
Ziqing04 <163233688+Ziqing04@users.noreply.github.com>
2024-05-04 22:58:47 +0800

    Update mainFile.c

--- mainFile.c
+++ mainFile.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #pragma warning(disable:4996)
-
+int getValidChoice(int lowerBound, int upperBound);
 void main(){
 
   printf("LOGO");
   
   
+}
+int getValidChoice(int lowerBound, int upperBound) {
+	char input[10];
+	int choice;
+	do {
+		
+		scanf("%s", input);	
+	
+		//convert the input to integer for choice
+		choice = atoi(input);
+		if (choice < lowerBound || choice > upperBound) {
+			printf("Invalid choice. Please enter a number between %d and %d: ", lowerBound, upperBound);
+		}
+		else {
+			return choice;
+		}
+	} while (1);
 }
